d6735c27c6de1159f0ba21f907dc17acb194ad8e
hyew0nn <hyewon-0607@naver.com>
2025-11-04 23:01:00 +0900

    chore: project init setting

---
+++ src/main/java/com/modufit/ModuFitApplication.java
@@ -0,0 +1,13 @@
+package com.modufit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/modufit/ModuFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modufit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
